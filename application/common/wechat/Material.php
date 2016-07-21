d90873ab083262499097d4e6d56bbb94864ab11e
DeadSoul <zygtzxz@outlook.com>
2016-07-21 13:20:59 +0800

    图文消息同步到微信素材

---
+++ application/common/wechat/Material.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DeadSoul
+ * Date: 2016/7/21
+ */
+
+namespace app\common\wechat;
+
+use EasyWeChat\Core\Exception;
+use EasyWeChat\Foundation\Application;
+use EasyWeChat\Message\Article;
+
+Class Material {
+
+    private $app;
+    private $material;
+
+    public function __construct () {
+        $config = config('wechat');
+        $this->app = new Application($config);
+        $this->material = $this->app->material;
+    }
+
+    /**
+     * @param $path
+     * @return array ['media_id','url']
+     * 用于上传图片
+     */
+    public function imagePush ($path) {
+        try {
+            $res = $this->material->uploadImage($path);
+
+            $return = [
+                'status'   => 0,
+                'media_id' => $res->media_id,
+                'url'      => $res->url
+            ];
+
+        } catch (Exception $e) {
+            $errCode = $e->getCode();
+            $error   = new Error($errCode);
+
+            $return = [
+                'status'  => $errCode,
+                'message' => $error->getMsg()
+            ];
+        }
+
+        return $return;
+    }
+
+    /**
+     * @param $data
+     * @return array
+     * 符合格式的数组
+     */
+    public function articlePush ($data) {
+        $article = $this->articleHandle($data);
+        $res = $this->material->uploadArticle($article);
+        $return = [
+            'status'   => 0,
+            'media_id' => $res->media_id
+        ];
+
+
+        return $return;
+    }
+
+
+    private function articleHandle ($data) {
+        if (!empty($data ['title'])) {
+            $data ['content'] = $this->contentHandle($data ['content']);
+
+            return new Article($data);
+        } else {
+            $i = 0;
+            $return = [];
+            foreach ($data as $value) {
+                $value ['content'] = $this->contentHandle($value ['content']);
+                $return [$i] = new Article($value);
+                $i++;
+            }
+            return $return;
+        }
+    }
+
+    private function contentHandle ($content) {
+        preg_match_all('/src=[\"|\']((.*).(gif|jpg|jpeg|bmp|png))/isU',$content,$match);
+        $imageList = array_unique($match [1]);
+        foreach ($imageList as $value) {
+            $url = $this->articleImagePush($value);
+
+            $content = str_replace($value,$url,$content);
+        }
+
+        return $content;
+    }
+
+    private function articleImagePush ($path) {
+        $res = $this->material->uploadArticleImage($path);
+        $url = $res->url;
+
+        return $url;
+    }
+
+    public function articlePreview ($media_id, $weChatAccount) {
+        try {
+            $this->app->broadcast->previewNewsByName($media_id, $weChatAccount);
+
+            $return = ['status' => 0];
+        } catch (Exception $e) {
+            $errCode = $e->getCode();
+            $error   = new Error($errCode);
+
+            $return = [
+                'status'  => $errCode,
+                'message' => $error->getMsg()
+            ];
+        }
+
+        return $return;
+    }
+
+}
--- application/home/controller/Index.php
+++ application/home/controller/Index.php
@@ -1,7 +1,6 @@
 <?php
 namespace app\home\controller;
-use think\Request;
-use think\Session;
+use app\common\wechat\Material;
 
 /**
 * 首页
@@ -22,5 +21,27 @@ class Index extends Base
             dump(cookie('userInfo'));
             return '123';
         }
+    }
+
+    public function articleDemo () {
+        $data = [
+            'title' => 'test1',
+            'thumb_media_id' => 'lvu-Y-bENiM0bLMLnFpoX9DLbkDrezAVqN_VawYpt3o',
+            'author' => 'DeadSoul',
+            'digest' => 'This is a test message',
+            'show_cover_pic' => 0,
+            'content' => '<p style="padding-left: 30px;">因为urlopen返回的是一个文件对象，所以我们可以通过read()等文件读取方法来操作里面的内容</p>
+<p style="padding-left: 30px;">除此之外，urllib提供了三个附加方法geturl(),getcode().info()</p>
+<p style="padding-left: 30px;">geturl()：这个方法返回的是最后所访问的url，比如</p><img src="public/upload/20160720/571ee9e36a7df678b2d5ff8bdaa768b1.jpg"><img src="public/upload/20160720/fa3a62889c9fa66060115caf4666d97f.jpg">',
+            'content_source_url' => 'http://www.baidu.com'
+        ];
+
+
+        $material = new Material();
+        $res = $material->articlePush($data);
+        //多图文消息用法$res = $material->articlePush([$data,$data,.....]);
+
+        dump($res);
+        return '';
     }
 }
