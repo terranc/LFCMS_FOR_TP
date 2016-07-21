f1590d1db7eaaa645a99bff0a8ee4e79da4dc7fc
DeadSoul <zygtzxz@outlook.com>
2016-07-21 14:31:10 +0800

    注释更新，更新图文消息方法补全

--- application/common/wechat/Material.php
+++ application/common/wechat/Material.php
@@ -23,9 +23,11 @@ Class Material {
     }
 
     /**
-     * @param $path
-     * @return array ['media_id','url']
      * 用于上传图片
+     *
+     * @param $path
+     *
+     * @return array ['media_id','url']
      */
     public function imagePush ($path) {
         try {
@@ -51,22 +53,40 @@ Class Material {
     }
 
     /**
+     * 用于将文章推送至微信服务器
+     *
      * @param $data
+     *
      * @return array
-     * 符合格式的数组
      */
     public function articlePush ($data) {
-        $article = $this->articleHandle($data);
-        $res = $this->material->uploadArticle($article);
-        $return = [
-            'status'   => 0,
-            'media_id' => $res->media_id
-        ];
+        try {
+            $article = $this->articleHandle($data);
+            $res = $this->material->uploadArticle($article);
+            $return = [
+                'status' => 0,
+                'media_id' => $res->media_id
+            ];
+        } catch (Exception $e) {
+            $errCode = $e->getCode();
+            $error   = new Error($errCode);
 
+            $return = [
+                'status'  => $errCode,
+                'message' => $error->getMsg()
+            ];
+        }
 
         return $return;
     }
 
+    /**
+     * 根据media_id删除永久素材
+     *
+     * @param $media_id
+     *
+     * @return array
+     */
     public function materialDel ($media_id) {
         try {
             $this->material->delete($media_id);
@@ -85,6 +105,68 @@ Class Material {
         return $return;
     }
 
+    /**
+     * 用于图文消息的更新
+     *
+     * @param string $media_id  已经上传的图文消息的media_id
+     * @param array $data
+     * @param int $index 多图文消息时，需要更新的图文消息所在位置，从0开始
+     *
+     * @return array
+     */
+    public function articleUpdate ($media_id, $data, $index = 0) {
+        try {
+            $article = $this->articleHandle($data);
+            $this->material->updateArticle($media_id, $article, $index);
+            $return = [
+                'status'   => 0,
+            ];
+        } catch (Exception $e) {
+            $errCode = $e->getCode();
+            $error   = new Error($errCode);
+
+            $return = [
+                'status'  => $errCode,
+                'message' => $error->getMsg()
+            ];
+        }
+
+        return $return;
+    }
+
+    /**
+     * 用于统计已经上传的所有素材的数量
+     *
+     * @return array
+     */
+    public function materialCount () {
+        try {
+            $res = $this->material->stats();
+
+            $return = [
+                'voice' => $res->voice_count,
+                'video' => $res->video_count,
+                'image' => $res->image_count,
+                'news'  => $res->news_count
+            ];
+        } catch (Exception $e) {
+            $errCode = $e->getCode();
+            $error   = new Error($errCode);
+
+            $return = [
+                'status'  => $errCode,
+                'message' => $error->getMsg()
+            ];
+        }
+        return $return;
+    }
+
+    /**
+     * 图文消息处理
+     *
+     * @param $data
+     * @return array|Article
+     */
     private function articleHandle ($data) {
         if (!empty($data ['title'])) {
             $data ['content'] = $this->contentHandle($data ['content']);
@@ -102,6 +184,12 @@ Class Material {
         }
     }
 
+    /**
+     * 用于处理图文消息内容中的图片，正则匹配img标签中的图片，并上传至微信服务器后替换
+     *
+     * @param $content
+     * @return mixed
+     */
     private function contentHandle ($content) {
         preg_match_all('/src=[\"|\']((.*).(gif|jpg|jpeg|bmp|png))/isU',$content,$match);
         $imageList = array_unique($match [1]);
@@ -114,6 +202,12 @@ Class Material {
         return $content;
     }
 
+    /**
+     * 用于图文消息内部的图片上传，返回url
+     *
+     * @param $path
+     * @return string 图片的url，用于替换
+     */
     private function articleImagePush ($path) {
         $res = $this->material->uploadArticleImage($path);
         $url = $res->url;
@@ -121,9 +215,22 @@ Class Material {
         return $url;
     }
 
-    public function articlePreview ($media_id, $weChatAccount) {
+    /**
+     * 用于图文消息的预览,通过option控制是openid还是预览人微信账号
+     * option留空时为微信账号，非0时为openid
+     *
+     * @param $media_id
+     * @param $user
+     * @param int $option
+     * @return array
+     */
+    public function articlePreview ($media_id, $user, $option = 0) {
         try {
-            $this->app->broadcast->previewNewsByName($media_id, $weChatAccount);
+            if ($option == 0) {
+                $this->app->broadcast->previewNewsByName($media_id, $user);
+            } else {
+                $this->app->broadcast->previewNews($media_id, $user);
+            }
 
             $return = ['status' => 0];
         } catch (Exception $e) {
--- application/home/controller/Index.php
+++ application/home/controller/Index.php
@@ -44,4 +44,24 @@ class Index extends Base
         dump($res);
         return '';
     }
+
+    public function articleUpdate () {
+        $data = [
+            'title' => 'test1',
+            'thumb_media_id' => 'lvu-Y-bENiM0bLMLnFpoX9DLbkDrezAVqN_VawYpt3o',
+            'author' => 'DeadSoul',
+            'digest' => 'This is a test message',
+            'show_cover_pic' => 0,
+            'content' => '<p style="padding-left: 30px;">因为urlopen返回的是一个文件对象，所以我们可以通过read()等文件读取方法来操作里面的内容</p>
+<p style="padding-left: 30px;">除此之外，urllib提供了三个附加方法geturl(),getcode().info()</p>
+<p style="padding-left: 30px;">geturl()：这个方法返回的是最后所访问的url，比如</p><img src="public/upload/20160720/571ee9e36a7df678b2d5ff8bdaa768b1.jpg"><img src="public/upload/20160720/fa3a62889c9fa66060115caf4666d97f.jpg">',
+            'content_source_url' => 'http://www.baidu.com'
+        ];
+
+        $material = new Material();
+        $res = $material->articleUpdate('lvu-Y-bENiM0bLMLnFpoXwIqUupbOMHC6AbHZznhwDU',$data,2);
+        //如果是单图文消息，无需填写最后的数字，多图文消息需要标明位置，从0开始
+        dump($res);
+        return ;
+    }
 }
